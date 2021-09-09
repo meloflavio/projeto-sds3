14368b5976d1384a6001a4c192a49465892aeafb
Flavio Melo <meloflavio@gmail.com>
2021-09-09 16:25:51 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/meloflavio" target="_blank" rel="noreferrer">Flávio Melo</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/fl.melo" target="_blank" rel="noreferrer">@fl.melo</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
