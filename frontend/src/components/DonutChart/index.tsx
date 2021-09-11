600d2f3d0c13384c17bb4bdb0cf4166abdc380a0
Flavio Melo <meloflavio@gmail.com>
2021-09-10 22:53:31 -0300

    DataTable integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -8,6 +8,7 @@ type ChartData = {
     labels: string[];
     series: number[];
 }
+
 const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
@@ -22,11 +23,6 @@ const DonutChart = () => {
                 setChartData({ labels: myLabels, series: mySeries});
             });
     }, []);
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
 
     const options = {
         legend: {
